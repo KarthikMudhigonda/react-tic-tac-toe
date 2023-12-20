aced1bebf9d9b0a9f4276c09703a88c9529eac5c
karthik <karthikmudhigonda1@gmail.com>
2023-12-20 11:36:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import GameBorad from "./components/GameBoard";
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1"  symbol="X"/>
+          <Player initialName="Player 2"  symbol="O"/>
+        </ol>
+        <GameBorad/>
+      </div>
+      LOG
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    // setIsEditing(isEditing ? false : true);
+    // setIsEditing(!isEditing); react bts scheduling states, not performed instantly.
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = 'Edit';
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = 'Save';
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
